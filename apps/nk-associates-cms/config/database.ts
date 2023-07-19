94fa318b0713d4bd718103097f8f901562f3829d
Abdul Hai <abdul@bluemeg.com>
2023-07-20 03:39:54 +0500

    futuristic ssl stuff

--- apps/nk-associates-cms/config/database.ts
+++ apps/nk-associates-cms/config/database.ts
@@ -1,33 +1,16 @@
-import path from "path";
-
-export default ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: false//JSON.parse(env("DATABASE_SSL_SELF", false)),
-        // ssl: false, // Set SSL to false to disable SSL for PostgreSQL
-        // schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: {
+        rejectUnauthorized: false, // For self-signed certificates
+      },
     },
-  };
-};
+    debug: false,
+  },
+});
