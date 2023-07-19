be0d5dccd35a292040aa33836a92a15116f5a1ac
Abdul Hai <abdul@bluemeg.com>
2023-07-20 03:22:40 +0500

    ssl false

--- apps/nk-associates-cms/config/database.ts
+++ apps/nk-associates-cms/config/database.ts
@@ -12,7 +12,7 @@ export default ({ env }) => {
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: JSON.parse(env("DATABASE_SSL_SELF", false)),
+        ssl: false//JSON.parse(env("DATABASE_SSL_SELF", false)),
         // ssl: false, // Set SSL to false to disable SSL for PostgreSQL
         // schema: env("DATABASE_SCHEMA", "public"),
       },
