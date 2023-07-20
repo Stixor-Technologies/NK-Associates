2528c99ed31a308937736835fc9c7ac9c0a38dc8
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-07-20 17:22:06 +0500

    added properties page

---
+++ apps/nk-associates-frontend/app/properties/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Properties = () => {
+  return (
+    <div className=''>
+        
+    </div>
+  )
+}
+
+export default Properties
