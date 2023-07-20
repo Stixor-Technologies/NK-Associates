837ec1f8746920fb982fda4834d40f2d38bfb474
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-07-20 14:14:43 +0500

    defined types for button

--- apps/nk-associates-frontend/components/button/button.tsx
+++ apps/nk-associates-frontend/components/button/button.tsx
@@ -1,12 +1,13 @@
 import React, {FC} from 'react'
 
 interface ButtonProps {
-   text: string
+   text: string,
+   type?: "solid" | "transparent" | "inverted",
 }
 
-const Button:FC<ButtonProps> = ({text}) => {
+const Button:FC<ButtonProps> = ({text, type = "default"}) => {
   return (
-    <button>{text}</button>
+    <button className={`rounded-full px-4 py-2 w-56 lg:py-3 text-lg font-vietnam-pro ${type === 'solid' ? 'bg-red-700' : type === 'inverted' ? 'bg-green-200' : 'bg-transparent'}`}>{text}</button>
   )
 }
 
