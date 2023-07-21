afa206e3b9baf64fc6a18aaaddcc715f5557cae8
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-07-21 22:14:31 +0500

    added the correct fonts

--- apps/nk-associates-frontend/app/properties/page.tsx
+++ apps/nk-associates-frontend/app/properties/page.tsx
@@ -5,14 +5,14 @@ const Properties = () => {
   const arr = [1, 2, 3, 4, 5];
   return (
     <section className=" h-full bg-right-top bg-no-repeat bg-[#f5f5f5] md:bg-[url('/assets/images/bg-property.svg')]">
-      <div className="container mx-auto px-4">
-        <div className="mt-12 text-center md:mt-20">
-          <h2 className="py-3 text-3xl text-nk-black md:text-5xl">Property</h2>
-          <p className="py-2 text-sm text-nk-black md:text-xl">
+      <div className="container mx-auto px-4 py-12 md:py-24">
+        <div className="text-center">
+          <h2 className="py-3 text-3xl font-metropolis-bold text-nk-black md:py-6 md:text-5xl">Property</h2>
+          <p className="py-2 text-sm font-metropolis-light text-nk-black md:text-xl">
             Unlock Your Perfect Property, Find, Buy, Rent with Ease
           </p>
         </div>
-        <div className="grid gap-4 mt-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid gap-y-12 gap-x-7 py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {arr.map((a) => (
             <PropertyCard key={a} />
           ))}
--- apps/nk-associates-frontend/components/button/properties/property-card.tsx
+++ apps/nk-associates-frontend/components/button/properties/property-card.tsx
@@ -10,7 +10,7 @@ const PropertyCard = () => {
       <Link href={"#"} className="rounded-xl">
         <Image src={"/card_img.svg"} width={400} height={400} alt="" />
       </Link>
-      <div className="my-2 flex justify-between">
+      <div className="my-3 flex items-center justify-between">
         <div className="flex items-center gap-2">
           <span className="rounded-full bg-white px-4 py-1 text-sm font-medium text-nk-gray shadow-lg md:text-xs">
             Residential
@@ -22,19 +22,19 @@ const PropertyCard = () => {
 
         <div className="flex items-center gap-1">
           <Image src={Area_Icon} width={13} height={13} alt="" />
-          <span className="text-sm text-nk-grey md:text-[0.625rem]">
+          <span className="text-sm text-nk-grey font-metropolis-light md:text-[0.625rem]">
             902.3 Sq.Yds
           </span>
         </div>
       </div>
 
-      <h2 className="text-xl font-medium text-nk-black md:text-base">
+      <h2 className="text-xl text-nk-black md:text-base">
         Garden City Bahria Town
       </h2>
-      <p className="my-1 text-[1.375rem] font-bold md:text-lg">Rs. 17 lac</p>
+      <p className="my-1 text-[1.375rem] text-nk-black font-metropolis-bold md:text-lg">Rs. 17 lac</p>
       <div className="mt-1 flex items-center gap-2">
         <Image src={Area_Marker} width={12} height={18} alt="" />
-        <p className="text-sm text-nk-grey md:text-xs">
+        <p className="text-sm text-nk-grey font-metropolis-light md:text-xs">
           Garden City phase 7 Bahria Town
         </p>
       </div>
