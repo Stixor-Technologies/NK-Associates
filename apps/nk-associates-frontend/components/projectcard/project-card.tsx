24e76b05ca61d569d1856ad5de95cd048fc7c55c
Hashaam Ejaz <Hashaamejaz@gmail.com>
2023-07-24 13:04:10 +0500

    Project Card Completed

--- apps/nk-associates-frontend/components/projectcard/project-card.tsx
+++ apps/nk-associates-frontend/components/projectcard/project-card.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
-import LocationIcon from "../../app/assets/images/LocationIcon";
+import LocationIcon from "../../app/assets/images/LocationIcon.svg";
+import LocationIconSecondary from "../../app/assets/images/LocationIconSecondary.svg";
+import ProjectImage from "../../app/assets/images/ProjectSampleImage.png";
 
 const ProjectCard = ({
   image = "/../../favicon.ico",
@@ -10,43 +12,81 @@ const ProjectCard = ({
   coveredArea = "11,000.46 Sq. ft",
   location = "Main Boulevard, Bahria Enclave, Islamabad",
   propertyDescription = "A beautiful plot located in a beautiful location just give us your god damn money",
+  propertyType = "Commercial",
+  primaryColor = false,
 }) => {
+  const backgroundColor = primaryColor ? "bg-nk-gradient-red-one" : "bg-white";
+  const textColor = primaryColor ? "text-white" : "text-black";
+  const locationIconComponent = primaryColor ? (
+    <Image src={LocationIconSecondary.src} width={30} height={30} alt="" />
+  ) : (
+    <Image src={LocationIcon.src} width={30} height={30} alt="" />
+  );
+
   return (
-    <div className=" flex flex-col rounded-2xl bg-red-300 shadow-md md:flex-row-reverse md:rounded-3xl">
-      <div className="relative h-80 w-full  bg-red-700 md:w-1/2">
+    <div
+      className={`m-5 flex flex-col rounded-2xl shadow-md md:flex-row-reverse md:rounded-3xl ${backgroundColor} ${textColor}`}
+    >
+      <div className="relative h-80 w-full rounded-2xl   md:flex md:h-auto md:w-1/2 lg:h-auto ">
         <Image
-          src={image}
+          src={ProjectImage.src}
           alt="Card Image"
           layout="fill"
-          objectFit="contain"
+          objectFit="cover"
           className="rounded-lg"
         />
       </div>
-      <div className=" mx-6 my-6 md:mt-0 md:w-1/2">
+      <div className=" m-6 flex-1 md:m-8  md:w-1/2 ">
         <div className=" flex flex-col">
           <div className="flex w-full flex-row">
-            <h1 className="mb-2 text-5xl font-bold">{propertyName}</h1>
+            <h1 className="mb-2 font-metropolis-bold text-5xl">
+              {propertyName}
+            </h1>
           </div>
           <div className="flex w-full flex-row">
-            <h3 className="mb-2 text-base font-normal">
-              Plot Size: {plotSize}
+            {propertyDescription && (
+              <div className="hidden w-full flex-row  md:flex">
+                <h3 className="mb-2 font-metropolis text-base">
+                  <span className=" font-metropolis">
+                    {propertyDescription}
+                  </span>
+                </h3>
+              </div>
+            )}
+          </div>
+          <div className="flex w-full flex-row">
+            <h3 className="mb-2 font-metropolis text-base">
+              Plot Size:
+              <span className="mx-3 font-metropolis-bold">{plotSize}</span>
             </h3>
           </div>
           <div className="flex w-full flex-row">
-            <h3 className="mb-2 text-base font-normal">Plot No: {plotNo}</h3>
+            <h3 className="mb-2 font-metropolis text-base">
+              Plot No:
+              <span className="mx-3 font-metropolis-bold">{plotNo}</span>
+            </h3>
           </div>
           <div className="flex w-full flex-row">
-            <h3 className="mb-2 text-base font-normal">
+            <h3 className="mb-2 font-metropolis text-base">
               Covered Area:{" "}
-              <span className="bg-white font-bold">{coveredArea}</span>
+              <span className="font-metropolis-bold">{coveredArea}</span>
             </h3>
           </div>
           <div className="flex w-full flex-row">
-            <div className="flex w-1/12 flex-col items-center justify-center bg-red-50">
-              <LocationIcon />
-            </div>
-            <div className="flex w-11/12 flex-col bg-red-200">
-              {propertyDescription}
+            {propertyType && (
+              <div className="hidden md:my-6 md:flex md:flex-row md:justify-center md:rounded-full md:bg-[#F5F5F5]">
+                <h3 className="mb-2 flex items-center font-metropolis text-base">
+                  <span className="inline-block max-w-max px-4 pt-2 font-metropolis text-nk-gray">
+                    {propertyType}
+                  </span>
+                </h3>
+              </div>
+            )}
+          </div>
+          <div className="flex w-full flex-row">
+            {locationIconComponent}
+            <div className="mx-4 flex w-11/12 flex-col justify-center font-metropolis">
+              {location}
             </div>
           </div>
         </div>
