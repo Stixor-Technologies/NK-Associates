7767f083e4e45b857b4699f78ed4f565022ce5dd
Hashaam Ejaz <Hashaamejaz@gmail.com>
2023-07-24 17:44:41 +0500

    Final Changes to component

--- apps/nk-associates-frontend/components/projectcard/project-card.tsx
+++ apps/nk-associates-frontend/components/projectcard/project-card.tsx
@@ -4,24 +4,33 @@ import LocationIcon from "../../app/assets/images/LocationIcon.svg";
 import LocationIconSecondary from "../../app/assets/images/LocationIconSecondary.svg";
 import ProjectImage from "../../app/assets/images/ProjectSampleImage.png";
 
-const ProjectCard = ({
-  image = "/../../favicon.ico",
-  propertyName = "Regent one",
-  plotSize = "4.11 kanal",
-  plotNo = 1,
-  coveredArea = "11,000.46 Sq. ft",
-  location = "Main Boulevard, Bahria Enclave, Islamabad",
-  propertyDescription = "A beautiful plot located in a beautiful location just give us your god damn money",
-  propertyType = "Commercial",
-  primaryColor = false,
+interface ProjectCardProps {
+  image: string;
+  propertyName: string;
+  plotSize: string;
+  plotNo: number;
+  coveredArea: string;
+  location: string;
+  propertyDescription?: string;
+  propertyType: string;
+  primaryColor?: boolean;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  image,
+  propertyName,
+  plotSize,
+  plotNo,
+  coveredArea,
+  location,
+  propertyDescription,
+  propertyType,
+  primaryColor = true,
 }) => {
-  const backgroundColor = primaryColor ? "bg-nk-gradient-red-one" : "bg-white";
+  const backgroundColor = primaryColor
+    ? "bg-gradient-to-b bg-nk-gradient-red-one to-nk-gradient-red-two"
+    : "bg-white";
   const textColor = primaryColor ? "text-white" : "text-black";
-  const locationIconComponent = primaryColor ? (
-    <Image src={LocationIconSecondary.src} width={30} height={30} alt="" />
-  ) : (
-    <Image src={LocationIcon.src} width={30} height={30} alt="" />
-  );
 
   return (
     <div
@@ -29,7 +38,7 @@ const ProjectCard = ({
     >
       <div className="relative h-80 w-full rounded-2xl   md:flex md:h-auto md:w-1/2 lg:h-auto ">
         <Image
-          src={ProjectImage.src}
+          src={image}
           alt="Card Image"
           layout="fill"
           objectFit="cover"
@@ -84,7 +93,16 @@ const ProjectCard = ({
             )}
           </div>
           <div className="flex w-full flex-row">
-            {locationIconComponent}
+            <div className="mx-4 flex w-1/12 flex-col justify-center font-metropolis">
+              <Image
+                src={
+                  primaryColor ? LocationIconSecondary.src : LocationIcon.src
+                }
+                width={30}
+                height={30}
+                alt=""
+              />
+            </div>
             <div className="mx-4 flex w-11/12 flex-col justify-center font-metropolis">
               {location}
             </div>
