b4d60253720ae4087500f519987f70842269e75d
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-07-25 14:49:46 +0500

    added initial icons

--- apps/nk-associates-frontend/app/properties/property-list/[id]/page.tsx
+++ apps/nk-associates-frontend/app/properties/property-list/[id]/page.tsx
@@ -1,5 +1,9 @@
-'use client'
-import React, {FC} from 'react'
+"use client";
+import React, { FC } from "react";
+import Image from "next/image";
+import Bath_Icon from "../../../../public/assets/icons/bath-icon.svg";
+import Bedroom_Icon from "../../../../public/assets/icons/bedrooms-icon.svg";
+import LinkButton from "../../../../components/button/link-button";
 
 interface PropertyDetailProps {
   params: {
@@ -7,11 +11,43 @@ interface PropertyDetailProps {
   };
 }
 
-const PropertyDetail: FC<PropertyDetailProps> = ({params: {id}}) => {
-  console.log(id)
+const PropertyDetail: FC<PropertyDetailProps> = ({ params: { id } }) => {
+  console.log(id);
   return (
-    <div>Detail</div>
-  )
-}
+    <section>
+      <div>
+        <div>
+          <h2>DHA-III Residential Plots</h2>
 
-export default PropertyDetail
+          <div>
+            <div className="flex items-center gap-1">
+              <Image src={Bath_Icon} width={13} height={13} alt="" />
+              <span className="text-nk-grey font-metropolis-light text-sm md:text-[0.625rem]">
+                7
+              </span>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <Image src={Bedroom_Icon} width={13} height={13} alt="" />
+              <span className="text-nk-grey font-metropolis-light text-sm md:text-[0.625rem]">
+                6
+              </span>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <Image src={Bath_Icon} width={13} height={13} alt="" />
+              <span className="text-nk-grey font-metropolis-light text-sm md:text-[0.625rem]">
+                4500 sq.ft
+              </span>
+            </div>
+          </div>
+         
+         <LinkButton text="Inquires" type="solid" navigateTo="#" />
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PropertyDetail;
