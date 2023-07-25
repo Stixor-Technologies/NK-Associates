3334c52bf257a328af35c0b6751925c8bc6dcb1d
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-07-25 10:57:55 +0500

    added bg image to tailwind config file

--- apps/nk-associates-frontend/app/properties/property-list/page.tsx
+++ apps/nk-associates-frontend/app/properties/property-list/page.tsx
@@ -3,7 +3,7 @@ import PropertyList from "../../../components/properties/property-list/property-
 
 const Properties = () => {
   return (
-    <section className="min-h-screen flex flex-col bg-right-top bg-no-repeat md:bg-[url('/assets/images/bg-property.svg')]">
+    <section className="min-h-screen flex flex-col bg-right-top bg-no-repeat md:bg-nk-bg">
       <div className="container mx-auto px-4 py-12 flex-1 flex flex-col md:py-24">
         <div className="text-center">
           <h2 className="py-3 font-metropolis-bold text-3xl text-nk-black md:py-6 md:text-5xl">
