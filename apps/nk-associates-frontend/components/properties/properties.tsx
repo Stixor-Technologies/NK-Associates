d1b01348be282996bef3cd81dd31bd1fe759e910
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-02 11:11:54 +0500

    integrated google maps

--- apps/nk-associates-frontend/components/properties/properties.tsx
+++ apps/nk-associates-frontend/components/properties/properties.tsx
@@ -1,65 +1,100 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import PropertyList from "./property-list";
 import PropertyMap from "./map-view-list";
 import Spinner from "../spinner";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { getProperties } from "../../utils/api-calls";
 import { Property } from "../../utils/types/types";
+import { MAP_KEY } from "../../utils/constants";
 import Map_Btn from "../../public/assets/icons/map-list-icon.svg";
-import List_Icon from "../../public/assets/icons/list-icon.svg"
+import List_Icon from "../../public/assets/icons/list-icon.svg";
 
+const libraries = ["places"];
+
+const center = {
+  lat: 43.653225,
+  lng: -79.383186,
+};
 const Properties = () => {
   const [isList, setIsList] = useState<boolean>(true);
-  const [properties, setProperties] = useState<Property[]>([]);
+  const [gridProperties, setGridProperties] = useState<Property[]>([]);
+  const [mapProperties, setMapProperties] = useState<Property[]>([]);
+
   const [total, setTotal] = useState<number | null>(null);
+  const [bounds, setBounds] = useState(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const fetchData = async () => {
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: MAP_KEY,
+  });
+
+  const fetchGridData = async () => {
     setIsLoading(true);
-    const resp = await getProperties(properties.length, 12);
+    const resp = await getProperties(gridProperties.length, 12);
     if (resp?.data) {
-      setProperties((prevProperties) => [...prevProperties, ...resp.data]);
+      setGridProperties((prevProperties) => [...prevProperties, ...resp.data]);
       setTotal(resp.meta.pagination.total);
     }
     setIsLoading(false);
   };
 
-  useEffect(() => {
-    fetchData();
+  const onMapLoad = useCallback((map) => {
+    // setBounds(map.getBounds().toJSON());
   }, []);
+
+  const onBoundsChanged = useCallback((map) => {
+    // setBounds(map.getBounds().toJSON());
+  }, []);
+
+  useEffect(() => {
+    if (isList && gridProperties.length === 0) {
+      console.log("Fetch Grid Data")
+      fetchGridData();
+    } else if (!isList && bounds && mapProperties.length === 0) {
+      console.log("fetch Map Data");
+    }
+    // fetchGridData();
+  }, [isList, bounds]);
 
   return (
     <>
       <button
-        className={`fixed bottom-16 left-1/2 z-10 flex items-center -translate-x-1/2 gap-2 rounded-full px-4 py-2 text-center text-sm capitalize text-nk-white transition-all duration-300 ease-in-out hover:shadow-lg hover:delay-100 md:gap-4 md:px-6 md:py-3 md:text-2xl ${isList ? "bg-gradient-to-b bg-nk-gradient-red-one to-nk-gradient-red-two" : "bg-nk-black"}`}
+        className={`fixed bottom-16 left-1/2 z-10 flex -translate-x-1/2 items-center gap-2 rounded-full px-4 py-2 text-center text-sm capitalize text-nk-white transition-all duration-300 ease-in-out hover:shadow-lg hover:delay-100 md:gap-4 md:px-6 md:py-3 md:text-2xl ${
+          isList
+            ? "bg-nk-gradient-red-one bg-gradient-to-b to-nk-gradient-red-two"
+            : "bg-nk-black"
+        }`}
         onClick={() => setIsList(!isList)}
       >
         <span>{`${isList ? "Show Map" : "Show List"}`}</span>
-          <Image
-            src={isList ? Map_Btn : List_Icon}
-            width={35}
-            height={35}
-            alt="properties-view"
-            className={`mx-auto ${isList ? "w-[1.375rem] md:w-[2.188rem]" : " w-4 md:w-[1.7rem]"} `}
-          />
+        <Image
+          src={isList ? Map_Btn : List_Icon}
+          width={35}
+          height={35}
+          alt="properties-view"
+          className={`mx-auto ${
+            isList ? "w-[1.375rem] md:w-[2.188rem]" : " w-4 md:w-[1.7rem]"
+          } `}
+        />
       </button>
 
       {isList ? (
         <>
-          {isLoading && properties.length === 0 ? (
+          {isLoading && gridProperties.length === 0 ? (
             <div className="flex flex-1">
               <Spinner />
             </div>
-          ) : properties && properties.length > 0 ? (
+          ) : gridProperties && gridProperties.length > 0 ? (
             <InfiniteScroll
-              dataLength={properties.length}
-              next={fetchData}
-              hasMore={total !== properties.length}
+              dataLength={gridProperties.length}
+              next={fetchGridData}
+              hasMore={total !== gridProperties.length}
               loader={isLoading && <Spinner />}
             >
-              <PropertyList properties={properties} />
+              <PropertyList properties={gridProperties} />
             </InfiniteScroll>
           ) : (
             <div className="flex flex-1 items-center justify-center text-nk-black">
@@ -68,7 +103,15 @@ const Properties = () => {
           )}
         </>
       ) : (
-        <PropertyMap />
+        // <PropertyMap />
+        <GoogleMap
+        zoom={10}
+        center={center}
+        onLoad={onMapLoad}
+        // onBoundsChanged={onBoundsChanged}
+      >
+        {/* <PropertyMap properties={mapProperties} /> */}
+      </GoogleMap>
       )}
     </>
   );
