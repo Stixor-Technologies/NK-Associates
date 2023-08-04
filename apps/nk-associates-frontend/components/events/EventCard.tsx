b92d7ece210165462ce4259cc005492af547f835
ahmadashrafstixor <ahmad.ashraf@stixor.com>
2023-08-04 11:17:37 +0500

    sorted on the basis of event_date

--- apps/nk-associates-frontend/components/events/EventCard.tsx
+++ apps/nk-associates-frontend/components/events/EventCard.tsx
@@ -26,121 +26,133 @@ const EventCard: React.FC<EventProps> = ({ data }) => {
     setIsOpen(true);
   };
 
+  // const sortedData = data?.sort((a, b) => {
+  //   const dateA = new Date(a?.attributes?.event_date);
+  //   const dateB = new Date(b?.attributes?.event_date);
+  //   return dateA - dateB; // Ascending order, for descending order use: dateB - dateA
+  // });
+
   return (
     <>
-      {data?.map((dataItem, index) => (
-        <div
-          key={index}
-          className="flex flex-1 flex-col overflow-hidden rounded-lg"
-        >
-          <div className="overflow-hidden rounded-lg">
-            <div className="slide-container relative mb-3 overflow-hidden rounded-lg">
-              <Swiper
-                grabCursor={true}
-                slidesPerView={1}
-                pagination={true}
-                modules={[Pagination]}
-                className="mySwiper h-[20rem] w-full md:h-[25rem]"
-              >
-                {dataItem?.attributes?.event_image?.data?.map(
-                  (imageData, index) => {
-                    return (
-                      <SwiperSlide key={index} className="h-full w-full">
-                        <Image
-                          src={`${BASE_URL}${imageData.attributes.url.trim()}`}
-                          alt="Slide Image"
-                          fill
-                          className="object-cover"
-                        />
-                      </SwiperSlide>
-                    );
-                  }
-                )}
-              </Swiper>
-            </div>
-
-            <div className="flex justify-between">
-              <div className="mb-3 line-clamp-1 max-w-full font-metropolis-bold text-2xl tracking-tight text-gray-900 md:max-w-[70%] md:text-3xl">
-                {dataItem?.attributes?.event_title}
+      {data
+        ?.sort((a, b) => {
+          const dateA: any = new Date(a?.attributes?.event_date);
+          const dateB: any = new Date(b?.attributes?.event_date);
+          return dateB - dateA; // Ascending order, for descending order use: dateB - dateA
+        })
+        .map((dataItem, index) => (
+          <div
+            key={index}
+            className="flex flex-1 flex-col overflow-hidden rounded-lg"
+          >
+            <div className="overflow-hidden rounded-lg">
+              <div className="slide-container relative mb-3 overflow-hidden rounded-lg">
+                <Swiper
+                  grabCursor={true}
+                  slidesPerView={1}
+                  pagination={true}
+                  modules={[Pagination]}
+                  className="mySwiper h-[20rem] w-full md:h-[25rem]"
+                >
+                  {dataItem?.attributes?.event_image?.data?.map(
+                    (imageData, index) => {
+                      return (
+                        <SwiperSlide key={index} className="h-full w-full">
+                          <Image
+                            src={`${BASE_URL}${imageData.attributes.url.trim()}`}
+                            alt="Slide Image"
+                            fill
+                            className="object-cover"
+                          />
+                        </SwiperSlide>
+                      );
+                    }
+                  )}
+                </Swiper>
               </div>
-              {dataItem?.attributes?.event_location && (
-                <div className="mb-3 mr-1 hidden max-w-[30%] gap-2 md:flex md:items-center ">
-                  <>
-                    <Image
-                      src={LocationMarker}
-                      alt="Location Bar"
-                      width={20}
-                      height={20}
-                      className="flex-shrink-0"
-                    />
-                  </>
-                  <div className="font-metropolis-light text-lg text-red-500">
-                    {dataItem?.attributes?.event_location}
-                  </div>
+
+              <div className="flex justify-between">
+                <div className="mb-3 line-clamp-1 max-w-full font-metropolis-bold text-2xl tracking-tight text-gray-900 md:max-w-[70%] md:text-3xl">
+                  {dataItem?.attributes?.event_title}
                 </div>
-              )}
-            </div>
-
-            <div className="mb-3 text-nk-gray underline">
-              Hosted by
-              <span className="text-nk-black">
-                {` ${dataItem?.attributes?.event_host}`}
-              </span>
-            </div>
-
-            <div className="mb-3 flex flex-col gap-2">
-              <div className="custom-scrollbar  line-clamp-2 h-12 overflow-hidden text-nk-black">
-                {dataItem?.attributes?.event_description}
+                {dataItem?.attributes?.event_location && (
+                  <div className="mb-3 mr-1 hidden max-w-[30%] gap-2 md:flex md:items-center ">
+                    <>
+                      <Image
+                        src={LocationMarker}
+                        alt="Location Bar"
+                        width={20}
+                        height={20}
+                        className="flex-shrink-0"
+                      />
+                    </>
+                    <div className="font-metropolis-light text-lg text-red-500">
+                      {dataItem?.attributes?.event_location}
+                    </div>
+                  </div>
+                )}
               </div>
 
-              <div
-                className="flex gap-1 text-nk-red"
-                onClick={() => {
-                  readMoreClick(dataItem);
-                }}
-              >
-                <div className="cursor-pointer">Read More</div>
-                <Image
-                  src={Arrow}
-                  alt="Location Bar"
-                  width={10}
-                  height={10}
-                  className="flex-shrink-0 -rotate-90 cursor-pointer accent-nk-red"
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-between">
-              <div className="mb-3 font-metropolis-bold text-nk-black underline">
-                Date:
-                <span className="font-metropolis-semibold text-nk-gray">
-                  {` ${dataItem?.attributes?.event_date
-                    .split("-")
-                    .reverse()
-                    .join("-")}`}
+              <div className="mb-3 text-nk-gray underline">
+                Hosted by
+                <span className="text-nk-black">
+                  {` ${dataItem?.attributes?.event_host}`}
                 </span>
               </div>
 
-              {dataItem?.attributes.event_location && (
-                <div className="mb-3 mr-1 flex items-center gap-2 md:hidden">
-                  <>
-                    <Image
-                      src={LocationMarker}
-                      alt="Location Bar"
-                      width={20}
-                      height={20}
-                      className="flex-shrink-0"
-                    />
-                  </>
-                  <div className="text-lg text-red-500">
-                    {dataItem?.attributes?.event_location}
-                  </div>
+              <div className="mb-3 flex flex-col gap-2">
+                <div className="custom-scrollbar  line-clamp-2 h-12 overflow-hidden text-nk-black">
+                  {dataItem?.attributes?.event_description}
                 </div>
-              )}
+
+                <div
+                  className="flex gap-1 text-nk-red"
+                  onClick={() => {
+                    readMoreClick(dataItem);
+                  }}
+                >
+                  <div className="cursor-pointer">Read More</div>
+                  <Image
+                    src={Arrow}
+                    alt="Location Bar"
+                    width={10}
+                    height={10}
+                    className="flex-shrink-0 -rotate-90 cursor-pointer accent-nk-red"
+                  />
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                <div className="mb-3 font-metropolis-bold text-nk-black underline">
+                  Date:
+                  <span className="font-metropolis-semibold text-nk-gray">
+                    {` ${dataItem?.attributes?.event_date
+                      .split("-")
+                      .reverse()
+                      .join("-")}`}
+                  </span>
+                </div>
+
+                {dataItem?.attributes.event_location && (
+                  <div className="mb-3 mr-1 flex items-center gap-2 md:hidden">
+                    <>
+                      <Image
+                        src={LocationMarker}
+                        alt="Location Bar"
+                        width={20}
+                        height={20}
+                        className="flex-shrink-0"
+                      />
+                    </>
+                    <div className="text-lg text-red-500">
+                      {dataItem?.attributes?.event_location}
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
       <EventModal
         open={isOpen}
         onClose={() => {
