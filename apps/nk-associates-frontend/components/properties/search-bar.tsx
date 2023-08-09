0df46d824920321ef6ac1ab96bbffdb22fc9726f
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-09 14:30:57 +0500

    added button styling

--- apps/nk-associates-frontend/components/properties/search-bar.tsx
+++ apps/nk-associates-frontend/components/properties/search-bar.tsx
@@ -5,15 +5,15 @@ import ArrowDown from "../../public/assets/icons/arrow-down.svg"
 const SearchBar = () => {
   return (
     <div>
-        <button className="bg-nk-white rounded-l-xl">
-            <span>
+        <button className="bg-nk-white rounded-l-xl   py-4 px-6">
+            <span className=" text-nk-gray md:text-lg">
                 Property Type
             </span>
-            <div>
+            <div className='flex'>
                 <span>
                     Any
                 </span>
-                <Image src={ArrowDown} width={35} height={35} alt='arrow-down' />
+                <Image src={ArrowDown} width={18} height={9} alt='arrow-down' />
             </div>
         </button>
     </div>
