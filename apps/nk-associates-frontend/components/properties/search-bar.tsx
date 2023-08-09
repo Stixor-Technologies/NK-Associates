c68d4fda63431c66f87d4ad08730187a338b041d
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-09 13:15:30 +0500

    added search bar component

---
+++ apps/nk-associates-frontend/components/properties/search-bar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>
+        <button>
+            <span>
+                Property Type
+                
+            </span>
+        </button>
+    </div>
+  )
+}
+
+export default SearchBar
