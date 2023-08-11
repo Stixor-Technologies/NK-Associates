e61d22afb877903c5634418e81b9ddff8dd2df1b
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-11 18:15:12 +0500

    check for build errors

--- apps/nk-associates-frontend/app/contact/page.tsx
+++ apps/nk-associates-frontend/app/contact/page.tsx
@@ -14,14 +14,14 @@ const ContactUs = async () => {
   const data: Contacts[] = await getOfficeAddress();
   const headOfficeAddress = await getHeadOffice();
   const categories = await getCategories();
-  const combinedAddresses: Contacts[] = [
-    ...data,
-    ...(headOfficeAddress ? [headOfficeAddress] : []),
-  ];
-  const headOfficeLocation: Location = {
-    lat: headOfficeAddress?.attributes?.latitude,
-    lng: headOfficeAddress?.attributes?.longitude,
-  };
+  // const combinedAddresses: Contacts[] = [
+  //   ...data,
+  //   ...(headOfficeAddress ? [headOfficeAddress] : []),
+  // ];
+  // const headOfficeLocation: Location = {
+  //   lat: headOfficeAddress?.attributes?.latitude,
+  //   lng: headOfficeAddress?.attributes?.longitude,
+  // };
 
   return (
     <section className="">
@@ -29,10 +29,10 @@ const ContactUs = async () => {
         <ContactForm categories={categories} />
 
         <div className="card-component mt-8 grid w-full gap-3 sm:grid-cols-2 lg:grid-cols-3 lg:gap-8">
-          <ContactDetails data={combinedAddresses} />
+          <ContactDetails data={data} />
         </div>
 
-        {headOfficeLocation?.lat && headOfficeLocation?.lng && (
+        {/* {headOfficeLocation?.lat && headOfficeLocation?.lng && (
           <div className="mt-14 w-full md:mt-24">
             <div className="flex w-full flex-col items-center justify-center">
               <div className="mb-5 text-center font-metropolis-bold text-[2rem] text-nk-black md:mb-8 md:text-4xl">
@@ -41,7 +41,7 @@ const ContactUs = async () => {
               <MapComponent locations={headOfficeLocation} />
             </div>
           </div>
-        )}
+        )} */}
       </div>
     </section>
   );
