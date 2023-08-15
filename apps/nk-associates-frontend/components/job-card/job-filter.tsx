9679383943a8e27b6bde19be063477f9b207b2d9
OmerKhanStix <omer.khan@stixor.com>
2023-08-15 12:59:21 +0500

    prettier formatting fix

--- apps/nk-associates-frontend/components/job-card/job-filter.tsx
+++ apps/nk-associates-frontend/components/job-card/job-filter.tsx
@@ -16,7 +16,7 @@ const JobFilter: FC<FilterProps> = ({
   return (
     <div className="relative m-2">
       <select
-        className="w-full appearance-none rounded-full border border-nk-gray px-4 py-3.5 pr-8 text-nk-gray focus:border-nk-red focus:outline-none"
+        className="border-nk-gray text-nk-gray focus:border-nk-red w-full appearance-none rounded-full border px-4 py-3.5 pr-8 focus:outline-none"
         value={selectedValue || ""}
         onChange={(e) => handleSelect(e.target.value || null)}
       >
@@ -29,7 +29,7 @@ const JobFilter: FC<FilterProps> = ({
       </select>
       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
         <svg
-          className="h-6 w-6 text-nk-gray"
+          className="text-nk-gray h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
