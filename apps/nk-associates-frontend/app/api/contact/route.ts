69cf6838c0737906554aef911938647e96a8ad10
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-16 17:31:45 +0500

    fixed env vars issue

--- apps/nk-associates-frontend/app/api/contact/route.ts
+++ apps/nk-associates-frontend/app/api/contact/route.ts
@@ -1,29 +1,30 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import sendgrid from "@sendgrid/mail";
-import { EMAIL_API, EMAIL_TO, EMAIL_FROM } from "../../../utils/constants";
 
-sendgrid.setApiKey(EMAIL_API);
+sendgrid.setApiKey(process.env.EMAIL_API_KEY);
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
-  const msg = {
-    to: EMAIL_TO,
-    from: EMAIL_FROM,
-    replyTo: data?.email,
-    subject: data.subject,
-    body: data.message,
-    html: `<div>
-    <p>You've got a new mail from ${data.name}, their email is: ${data.email}, their number is ${data?.phone} </p>
-    <p><span>Selected Category:</span> ${data.category} </p>
-    ${data.message}</div>`,
-  };
-
   try {
+    const msg = {
+      to: process.env.NEXT_SENDGRID_EMAIL_TO,
+      from: process.env.NEXT_SENDGRID_EMAIL_FROM,
+      replyTo: data?.email,
+      subject: data.subject,
+      body: data.message,
+      html: `<div>
+      <p>You've got a new mail from ${data.name}, their email is: ${data.email}, their number is ${data?.phone} </p>
+      <p><span>Selected Category:</span> ${data.category} </p>
+      ${data.message}</div>`,
+    };
     const res = await sendgrid.send(msg);
     return NextResponse.json(res[0].statusCode);
   } catch (error) {
-    console.error("error", error);
-    return NextResponse.json({ message: error }, { status: error?.code });
+    console.error("error", error.message);
+    return NextResponse.json(
+      { message: error.message },
+      { status: error?.code || 400 },
+    );
   }
 }
