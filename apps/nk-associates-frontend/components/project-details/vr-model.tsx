421b7302c5539fb6d458b90cf3909c63d0884284
M. Tallal Azam <m.tallalazam@Ms-MacBook-Pro.local>
2023-08-16 13:40:33 +0500

    Added GLRFLoader

--- apps/nk-associates-frontend/components/project-details/vr-model.tsx
+++ apps/nk-associates-frontend/components/project-details/vr-model.tsx
@@ -5,10 +5,19 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
+import SampleGLB from "../../../public/assets/sample.glb";
+
 const VRModel = () => {
+  const gltf = useLoader(GLTFLoader, SampleGLB.src);
   return (
     <section>
-      <p>VR Modal</p>
+      <Canvas style={{ background: "#171717" }}>
+        <primitive
+          object={gltf.scene}
+          position={[0, 1, 0]}
+          children-0-castShadow
+        />
+      </Canvas>
     </section>
   );
 };
