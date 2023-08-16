4737db1e2744235499b5c0c560c6eea1ac16e30a
OmerKhanStix <omer.khan@stixor.com>
2023-08-16 14:18:25 +0500

    styling fix

--- apps/nk-associates-frontend/app/not-found.tsx
+++ apps/nk-associates-frontend/app/not-found.tsx
@@ -5,16 +5,18 @@ import Navigation from "../components/navigation/navigation";
 import ErrorImage from "../public/assets/images/404.svg";
 export default function NotFound() {
   return (
-    <div className="container">
+    <div>
       <Navigation />
-      <div className="flex flex-col justify-center items-center bg-nk-background">
-        <Image src={ErrorImage} alt="Error Image" className="mt-24" />
-        <LinkButton
-          navigateTo="/"
-          type="solid"
-          text="Return Home"
-          className="bg-nk-red metro w-44 h-10 my-10"
-        />
+      <div className="bg-nk-background">
+        <div className="flex flex-col justify-center items-center container">
+          <Image src={ErrorImage} alt="Error Image" className="mt-24" />
+          <LinkButton
+            navigateTo="/"
+            type="solid"
+            text="Return Home"
+            className="bg-nk-red metro w-44 h-10 my-10"
+          />
+        </div>
       </div>
       <Footer />
     </div>
