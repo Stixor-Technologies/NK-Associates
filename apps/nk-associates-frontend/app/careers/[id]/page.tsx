1b08b54f2bb137f41a58420a8c75e253dc2e4537
Hashaam Ejaz <Hashaamejaz@gmail.com>
2023-08-16 10:39:34 +0500

    Initial work on NK-25

--- apps/nk-associates-frontend/app/careers/[id]/page.tsx
+++ apps/nk-associates-frontend/app/careers/[id]/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useEffect, useState } from "react";
 import LinkButton from "../../../components/button/link-button";
 import { getJobDetail } from "../../../utils/api-calls";
 import { Job } from "../../../utils/types/types";
+import JobModal from "../job-modal";
 
 interface JobDetailProps {
   params: {
@@ -8,8 +11,22 @@ interface JobDetailProps {
   };
 }
 
-async function JobDetail({ params: { id } }: JobDetailProps) {
-  const data: Job = await getJobDetail(id);
+function JobDetail({ params: { id } }: JobDetailProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const [data, setData] = useState<Job>();
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const result = await getJobDetail(id);
+    setData(result);
+  };
   const {
     title,
     description,
@@ -24,13 +41,13 @@ async function JobDetail({ params: { id } }: JobDetailProps) {
     location,
     skills,
     days,
-  } = data?.attributes;
-  const responsibilityArray = responsibilities.split("\n");
-  const skillArray = skills.split("\n");
+  } = data?.attributes || {};
+  const responsibilityArray = responsibilities?.split("\n");
+  const skillArray = skills?.split("\n");
 
   const formatTime = (time) => {
     const date = new Date();
-    const [hours, minutes] = time.split(":");
+    const [hours, minutes] = time?.split(":");
     date.setHours(hours);
     date.setMinutes(minutes);
     return date.toLocaleString("en-US", {
@@ -54,11 +71,19 @@ async function JobDetail({ params: { id } }: JobDetailProps) {
     }
   }
 
+  function openModal(): void {
+    setIsOpen(true);
+  }
+
   return (
-    <div className="px-6 text-center md:px-14">
-      <div className="pb-5 font-metropolis-bold text-3xl">{title}</div>
+    <div className=" block px-6 text-center md:px-14  ">
+      {/* <div className="absolute left-0 top-0 z-50 flex h-screen w-screen items-center justify-center overflow-hidden bg-[#00000046]"> */}
+      <JobModal onClose={closeModal} open={isOpen} />
+      {/* </div> */}
+
+      <div className="font-metropolis-bold pb-5 text-3xl">{title}</div>
       <div className="font-metropolis text-sm">{description}</div>
-      <div className="py-3 font-metropolis-semibold text-base">
+      <div className="font-metropolis-semibold py-3 text-base">
         No. of Positions: {positions}
       </div>
       <LinkButton
@@ -66,55 +91,58 @@ async function JobDetail({ params: { id } }: JobDetailProps) {
         navigateTo=""
         type="solid"
         className="h-10 w-80 border-2 "
+        clickEvent={openModal}
       />
-      <div className="py-4 pt-10 text-left font-metropolis-bold text-[1.75rem] leading-[2.1rem]">
+      <div className="font-metropolis-bold py-4 pt-10 text-left text-[1.75rem] leading-[2.1rem]">
         KEY RESPONSIBILITIES:
       </div>
       <div className="pb-4 text-left">
-        <ul className="list-disc pl-5 font-metropolis-thin">
-          {responsibilityArray.map((responsibility, index) => (
+        <ul className="font-metropolis-thin list-disc pl-5">
+          {responsibilityArray?.map((responsibility, index) => (
             <li key={index} className="py-1 text-sm">
-              {responsibility.trim()}
+              {responsibility?.trim()}
             </li>
           ))}
         </ul>
       </div>
-      <div className="py-4 text-left font-metropolis-bold text-[1.75rem] leading-[2.1rem]">
+      <div className="font-metropolis-bold py-4 text-left text-[1.75rem] leading-[2.1rem]">
         JOB SPECIFICATIONS
       </div>
-      <div className="pb-2 text-left font-metropolis">
-        <span className="font-metropolis-bold text-base text-nk-red">
+      <div className="font-metropolis pb-2 text-left">
+        <span className="font-metropolis-bold text-nk-red text-base">
           Qualification:{" "}
         </span>
         {qualification}
       </div>
-      <div className="text-left font-metropolis">
-        <span className="font-metropolis-bold text-base text-nk-red">
+      <div className="font-metropolis text-left">
+        <span className="font-metropolis-bold text-nk-red text-base">
           Experience:{" "}
         </span>
         {experience}
       </div>
-      <div className="py-4 text-left font-metropolis-bold text-[1.75rem] leading-[2.1rem]">
+      <div className="font-metropolis-bold py-4 text-left text-[1.75rem] leading-[2.1rem]">
         Skills Required
       </div>
-      <ul className="list-disc pl-5 text-left font-metropolis-thin text-nk-black">
-        {skillArray.map((skill, index) => (
+      <ul className="font-metropolis-thin text-nk-black list-disc pl-5 text-left">
+        {skillArray?.map((skill, index) => (
           <li key={index} className="py-1 text-sm">
-            {skill.trim()}
+            {skill?.trim()}
           </li>
         ))}
       </ul>
-      <div className="py-4 pt-10 text-left font-metropolis-bold text-[1.75rem] leading-[2.1rem]">
+      <div className="font-metropolis-bold py-4 pt-10 text-left text-[1.75rem] leading-[2.1rem]">
         Office Timings
       </div>
-      <div className="text-left font-metropolis-medium">
-        {formatTime(start)} to {formatTime(end)} ({displayDays(days)})
-      </div>
-      <div className="py-2  text-left font-metropolis-bold text-[1.75rem] leading-[2.1rem]">
+      {start && end && (
+        <div className="font-metropolis-medium text-left">
+          {formatTime(start)} to {formatTime(end)} ({displayDays(days)})
+        </div>
+      )}
+      <div className="font-metropolis-bold  py-2 text-left text-[1.75rem] leading-[2.1rem]">
         Location
       </div>
-      <div className="text-left  font-metropolis-medium">{location}</div>
-      <div className="py-3 font-metropolis-semibold text-base">
+      <div className="font-metropolis-medium  text-left">{location}</div>
+      <div className="font-metropolis-semibold py-3 text-base">
         No. of Positions: {positions}
       </div>
       <div className="pb-20">
@@ -122,7 +150,8 @@ async function JobDetail({ params: { id } }: JobDetailProps) {
           text="Apply Now"
           navigateTo=""
           type="solid"
-          className="h-10 w-80 border-2 "
+          className="h-10 w-80 border-2"
+          clickEvent={openModal}
         />
       </div>
     </div>
--- apps/nk-associates-frontend/utils/formik-schema.ts
+++ apps/nk-associates-frontend/utils/formik-schema.ts
@@ -20,3 +20,34 @@ export const ContactFormSchema = Yup.object().shape({
     .min(10, "Minimum 10 characters")
     .max(200, "Maximum 200 characters."),
 });
+
+export const JobFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Minimum 3 characters")
+    .max(20, "Maximum 25 characters")
+    .required("Please enter you name"),
+
+  father_name: Yup.string()
+    .min(3, "Minimum 3 characters")
+    .max(20, "Maximum 25 characters"),
+
+  email: Yup.string()
+    .matches(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i, "Invalid email address")
+    .required("Please enter your email address"),
+
+  phone: Yup.string()
+    .required("Please enter your phone number")
+    .matches(/^(\+92|0|92)[0-9]{10}$/, "Phone number is not valid"),
+
+  current_address: Yup.string()
+    .min(3, "Minimum 3 characters")
+    .max(100, "Maximum 100 characters")
+    .required("Please enter you current address"),
+
+  permanent_address: Yup.string()
+    .min(3, "Minimum 3 characters")
+    .max(100, "Maximum 100 characters")
+    .required("Please enter you permanent address"),
+
+  department: Yup.string().required("Please select category"),
+});
