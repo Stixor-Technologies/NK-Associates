e1a1e12a4ce281da35f946839565b15f53c5dc42
M. Tallal Azam <m.tallalazam@Ms-MacBook-Pro.local>
2023-08-16 10:07:59 +0500

    3d setup

---
+++ apps/nk-associates-frontend/components/project-details/vr-model.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useLayoutEffect } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const VRModel = () => {
+  return (
+    <section>
+      <p>VR Modal</p>
+    </section>
+  );
+};
+
+export default VRModel;
