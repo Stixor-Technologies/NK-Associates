287dbc545632271fa85c1a0c8343446c5e4a8301
M. Tallal Azam <m.tallalazam@Ms-MacBook-Pro.local>
2023-08-17 10:11:24 +0500

    Fixes for Model translate transition

--- apps/nk-associates-frontend/components/project-details/project-details-vr.tsx
+++ apps/nk-associates-frontend/components/project-details/project-details-vr.tsx
@@ -21,10 +21,10 @@ const ProjectDetailsVR = ({ modelURL = undefined }: PropTypes) => {
   const commonClasses =
     "transition-all relative w-full flex-shrink-0 h-[16.625rem] xs:h-[18.625rem] sm:h-[23.625rem] md:h-[28.625rem] lg:min-h-[35.375rem] xl:h-[39.375rem]";
   const stageClasses = `${commonClasses} ${
-    showModel ? "-translate-x-full" : ""
+    showModel ? "-translate-x-full" : "translate-x-0"
   } bg-red-300 bg-cover [background-image:url('/assets/images/bg-project.jpeg')]`;
   const modelClasses = `${commonClasses} ${
-    showModel ? "-translate-x-full" : ""
+    showModel ? "-translate-x-full" : "translate-x-0"
   }`;
 
   return (
