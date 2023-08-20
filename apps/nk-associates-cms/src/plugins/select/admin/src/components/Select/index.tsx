1d2fc93b405ece54c268fba903597666343b3dec
M. Tallal Azam <m.tallalazam@Ms-MacBook-Pro.local>
2023-08-20 06:21:14 +0500

    Plugin init

---
+++ apps/nk-associates-cms/src/plugins/select/admin/src/components/Select/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import {
+  HeaderLayout,
+  Typography,
+  Layout,
+  GridLayout,
+  Box,
+  Grid,
+  GridItem,
+} from "@strapi/design-system";
+
+const Select = (e) => {
+  console.log("select plugin", e);
+  return <div>Select</div>;
+};
+
+export default Select;
