cf197a6f8bd5303e395f8968810fcf35f1b4c006
Sanan bin Tahir <sananbintahir@gmail.com>
2023-08-21 13:11:23 +0500

    rename pm2 start script

--- apps/nk-associates-cms/scripts/start_server.sh
+++ apps/nk-associates-cms/scripts/start_server.sh
@@ -11,4 +11,4 @@ yarn install
 # Navigate to your project directory and start the app
 cd /home/ubuntu/app/apps/nk-associates-cms
 pm2 kill
-pm2 start ./scripts/start.sh --name nk-cms
+pm2 start ./scripts/pm2.sh --name nk-cms
