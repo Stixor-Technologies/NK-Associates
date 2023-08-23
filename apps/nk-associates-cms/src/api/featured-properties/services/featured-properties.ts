3a1bc68f2eed53e573be72f3a1d97923d31aaa54
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-23 10:32:57 +0500

    added featured properties in cms

---
+++ apps/nk-associates-cms/src/api/featured-properties/services/featured-properties.ts
@@ -0,0 +1,7 @@
+/**
+ * featured-properties service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::featured-properties.featured-properties');
