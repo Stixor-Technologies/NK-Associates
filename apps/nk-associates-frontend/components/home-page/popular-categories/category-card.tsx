4ecea36cc6765cc1ee21eec8ab73b1cac5100120
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-25 10:29:04 +0500

    added popular categories

--- apps/nk-associates-frontend/components/home-page/popular-categories/category-card.tsx
+++ apps/nk-associates-frontend/components/home-page/popular-categories/category-card.tsx
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { FC } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { PopularCategory } from "../../../utils/types/types";
+import { BASE_URL } from "../../../utils/constants";
 
-const CategoryCard = () => {
+type CategoryCardProps = {
+  category: PopularCategory;
+};
+
+const CategoryCard: FC<CategoryCardProps> = ({ category }) => {
+  const cat_image = category?.category_image?.data.attributes.url;
   return (
-    // <div className="max-w-[384px] min-w-[374px] w-full">
     <Link
       href={`/properties}`}
       className="group relative inline-block max-w-[384px] min-w-[374px] w-full"
     >
-      {/* before:content-[''] before:absolute before:block before:w-full before:h-full before:bg-slate-500 group-hover:before:z-10 */}
       <div className="aspect-w-1 aspect-h-1 group relative w-full max-w-[37.5rem] overflow-hidden h-[358px] rounded-xl  before:transition-all before:duration-500 before:ease-in-out before:content-[''] before:absolute before:block before:w-full before:h-full before:bg-overlay-black before:opacity-0 before:z-10 group-hover:before:opacity-100 ">
-        {/* group-hover:opacity-50 */}
         <Image
-          src={"/assets/images/bg-project.jpeg"}
+          src={`${BASE_URL}${cat_image || "/"}`}
           fill
-          alt=""
-          className="object-cover "
+          alt={`${category.category_name}-image`}
+          className="object-cover"
         />
       </div>
       <p className="absolute w-full bottom-6 text-nk-white text-2xl font-metropolis-bold bg-fade-red py-3 z-20">
-        <span className="mx-8">Commercial</span>
+        <span className="mx-8">{category.category_name}</span>
       </p>
     </Link>
-    // </div>
   );
 };
 
--- apps/nk-associates-frontend/components/home-page/popular-categories/popular-categories.tsx
+++ apps/nk-associates-frontend/components/home-page/popular-categories/popular-categories.tsx
@@ -3,21 +3,31 @@ import React, { useState, useEffect } from "react";
 import Spinner from "../../spinner";
 import { getPopularCategories } from "../../../utils/api-calls";
 import CategoryCard from "./category-card";
+import { PopularCategory } from "../../../utils/types/types";
 
 const PopularCategories = () => {
-  const [popularCategories, setPopularCategories] = useState([1, 2, 3]);
+  const [categories, setCategories] = useState<PopularCategory[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const transformResponse = (response) => {
+    const categoryKeys = Object.keys(response).filter(
+      (key) => key.includes("category") && !key.includes("image"),
+    );
+    return categoryKeys.map((categoryKey) => {
+      const imageKey = `${categoryKey}_image`;
+      return {
+        category_name: response[categoryKey],
+        category_image: response[imageKey],
+      };
+    });
+  };
 
   const fetchPopularCategories = async () => {
     setIsLoading(true);
     const resp = await getPopularCategories();
-    console.log(resp);
     if (resp?.data) {
-      const propertyArray = Object.keys(resp?.data?.attributes)
-        .filter((key) => !["createdAt", "updatedAt"]?.includes(key))
-        .map((key) => resp?.data?.attributes[key]?.data);
-
-      setPopularCategories(propertyArray);
+      const transformedData = transformResponse(resp?.data?.attributes);
+      setCategories(transformedData);
     }
     setIsLoading(false);
   };
@@ -28,22 +38,20 @@ const PopularCategories = () => {
 
   return (
     <>
-      {/* <CategoryCard /> */}
-
-      {popularCategories.length > 0 || isLoading ? (
+      {categories.length > 0 || isLoading ? (
         <div className="xl:container my-12 md:my-20">
           <h6 className="text-[2rem] text-nk-black text-center font-metropolis-semibold px-4 md:px-8 md:text-4xl xl:px-0">
             Popular Categories
           </h6>
 
-          {isLoading && popularCategories.length === 0 ? (
+          {isLoading && categories.length === 0 ? (
             <div className="min-h-[50vh] flex flex-1">
               <Spinner />
             </div>
           ) : (
             <div className="property-carousel flex flex-nowrap overflow-x-scroll px-4 py-9 gap-8 md:py-8 md:px-8 xl:px-0">
-              {popularCategories?.map((category, index: number) => (
-                <CategoryCard />
+              {categories?.map((category: PopularCategory, index: number) => (
+                <CategoryCard key={index} category={category} />
               ))}
             </div>
           )}
