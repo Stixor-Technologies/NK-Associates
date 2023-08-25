970b85803d720f0db75d03ff4fece498fd2e51e5
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-08-25 09:29:09 +0500

    addded animation for app section

--- apps/nk-associates-frontend/components/home-page/mobile-app.tsx
+++ apps/nk-associates-frontend/components/home-page/mobile-app.tsx
@@ -1,12 +1,16 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { getSocials } from "../../utils/api-calls";
 import FloatingMobile from "../../public/assets/images/floating-mobile.png";
 import GooglePlayIcon from "../../public/assets/icons/play-store-home.svg";
 import AppStoreIcon from "../../public/assets/icons/app-store-home.svg";
 import AppGalleryIcon from "../../public/assets/icons/app-gallery-home.svg";
-import Link from "next/link";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 type StoreLinks = {
   playstore: string;
@@ -16,6 +20,7 @@ type StoreLinks = {
 
 const NkApp = () => {
   const [storeLinks, setStoreLinks] = useState<StoreLinks>(null);
+  const ref = useRef<HTMLDivElement | null>(null);
 
   const fetchSocials = async () => {
     const resp = await getSocials();
@@ -34,8 +39,65 @@ const NkApp = () => {
     fetchSocials();
   }, []);
 
+  // useEffect(() => {
+  //   const tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: ref.current,
+  //       start: "top top",
+  //       markers: true,
+  //       toggleActions: "play none none none",
+  //     },
+  //   });
+  //   tl.from(".floating-mobile", {
+  //     opacity: 0,
+  //     x: -200,
+  //     y: 200,
+  //     scale: 0,
+  //     duration: 0.5,
+  //   });
+  //   tl.from(".link", {
+  //     opacity: 0,
+  //     xPercent: -60,
+  //     scale: 0,
+  //     stagger: 0.2,
+  //     ease: "circ.out",
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ref.current,
+        start: "top top",
+        markers: true,
+        toggleActions: "play none none none",
+      },
+    });
+    tl.from(".floating-mobile", {
+      opacity: 0,
+      x: -200,
+      y: 200,
+      scale: 0,
+      duration: 0.5,
+    }),
+      tl.to(".link", {
+        opacity: 1,
+        x: 0,
+        // scale: 1,
+        duration: 0.5,
+        stagger: 0.2,
+        ease: "circ.out",
+      }),
+      tl.to(".download-text", {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        ease: "circ.out",
+      });
+  }, []);
+
   return (
-    <div className="container my-20">
+    <div ref={ref} className="container my-20">
       <div className="flex flex-col sm:flex-row gap-8 sm:gap-0">
         <div className="sm:w-1/2 ">
           <Image
@@ -43,7 +105,7 @@ const NkApp = () => {
             width={470}
             height={470}
             alt="floating-mobile"
-            className="mx-auto"
+            className="floating-mobile mx-auto"
           />
         </div>
         <div className="sm:w-1/2 flex flex-col justify-center sm:items-start lg:px-14">
@@ -52,7 +114,7 @@ const NkApp = () => {
               href={storeLinks?.playstore || "#"}
               target={storeLinks?.playstore ? "_blank" : "_self"}
               rel="noopener noreferrer"
-              className="relative bg-black rounded-xl inline-flex items-center justify-center w-full h-[88px] sm:w-[300px] md:w-[350px] lg:w-[394px] transition-all duration-100 hover:scale-[1.01]"
+              className="link relative -translate-x-16 opacity-0 bg-black rounded-xl inline-flex items-center justify-center w-full h-[88px] sm:w-[300px] md:w-[350px] lg:w-[394px] transition-all duration-100 hover:scale-[1.01]"
             >
               <Image src={GooglePlayIcon} fill alt="play-store-link" />
             </Link>
@@ -60,7 +122,7 @@ const NkApp = () => {
             <Link
               href={storeLinks?.appstore || "#"}
               target={storeLinks?.appstore ? "_blank" : "_self"}
-              className="relative bg-black rounded-xl inline-flex items-center justify-center w-full h-[88px] sm:w-[300px] md:w-[350px] lg:w-[394px] transition-all duration-100 hover:scale-[1.01]"
+              className="link relative -translate-x-16 opacity-0 bg-black rounded-xl inline-flex items-center justify-center w-full h-[88px] sm:w-[300px] md:w-[350px] lg:w-[394px] transition-all duration-100 hover:scale-[1.01]"
             >
               <Image src={AppStoreIcon} fill alt="app-store-link" />
             </Link>
@@ -69,13 +131,13 @@ const NkApp = () => {
               href={storeLinks?.appgallery || "#"}
               target={storeLinks?.appgallery ? "_blank" : "_self"}
               rel="noopener noreferrer"
-              className="relative bg-black rounded-xl inline-flex items-center justify-center w-full h-[88px] sm:w-[300px] md:w-[350px] lg:w-[394px] transition-all duration-100 hover:scale-[1.01]"
+              className="link relative -translate-x-16  opacity-0 bg-black rounded-xl inline-flex items-center justify-center w-full h-[88px] sm:w-[300px] md:w-[350px] lg:w-[394px] transition-all duration-100 hover:scale-[1.01]"
             >
               <Image src={AppGalleryIcon} fill alt="app-gallery-link" />
             </Link>
           </div>
 
-          <div className="order-0 sm:order-none sm:max-w-[450px]">
+          <div className="download-text order-0 opacity-0 -translate-y-[60%] sm:order-none sm:max-w-[450px]">
             <p className="text-center text-2xl font-metropolis-semibold leading-tight text-nk-black sm:font-metropolis sm:text-left lg:text-4xl sm:mt-10">
               Download
               <span className="font-metropolis-bold"> NK Associates </span>
