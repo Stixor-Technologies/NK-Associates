54a854d124a36e43891b3d0583bb4e3922f48dea
OmerKhanStix <omer.khan@stixor.com>
2023-08-28 16:42:23 +0500

    initial layout

---
+++ apps/nk-associates-frontend/app/privacy-policy/page.tsx
@@ -0,0 +1,107 @@
+"use client";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="block p-8 text-nk-dark-gray title-trigger">
+      <h1 className="font-metropolis-bold text-center text-5xl md:p-[2rem] md:pb-[1rem]">
+        Privacy Policy
+      </h1>
+      <hr className="h-px mb-5 bg-nk-gray border"></hr>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        This policy describes the sources of your personal information that we
+        may collect from you or that you may provide to us, our purposes for
+        collecting and processing your information, and our practices for
+        collecting, using, maintaining, retaining, protecting, and disclosing
+        that information.
+      </p>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        If you have any questions or concerns about this privacy policy or your
+        personal information, please contact us using the details provided in
+        the Section – ‘How do you contact us?’.
+      </p>
+
+      <h2 className="font-metropolis-bold text-3xl mb-4">
+        Sources of Personal Data
+      </h2>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        This section outlines the sources from which we collect your personal
+        data.
+      </p>
+
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
+        Information We Collect and How We Use It
+      </h2>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        This section explains the types of information we collect about you and
+        how we use it for various purposes.
+      </p>
+
+      {/* wating on other sections */}
+
+      <h2 className="font-metropolis-bold text-3xl mb-4">Updates on Policy</h2>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        This policy may change from time to time. Your continued use of our
+        platforms after we make changes is deemed to be acceptance of those
+        changes, so please check the policy periodically for updates.
+      </p>
+
+      <h2 className="font-metropolis-bold text-3xl mb-4">How to Contact Us</h2>
+      <p className="font-metropolis text-base">
+        In case of any queries related to this policy, you can contact our Data
+        Privacy Office in one of the following ways.
+      </p>
+      <h2 className="font-metropolis-bold text-3xl mb-4">
+        Sources of Personal Data
+      </h2>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
+        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
+        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
+        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
+        venenatis. Donec fringilla augue non neque consectetur finibus sed et
+        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
+        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      </p>
+
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
+        Information We Collect and How We Use It
+      </h2>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
+        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
+        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
+        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
+        venenatis. Donec fringilla augue non neque consectetur finibus sed et
+        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
+        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      </p>
+
+      {/* wating on other sections */}
+
+      <h2 className="font-metropolis-bold text-3xl mb-4">Updates on Policy</h2>
+      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
+        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
+        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
+        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
+        venenatis. Donec fringilla augue non neque consectetur finibus sed et
+        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
+        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      </p>
+
+      <h2 className="font-metropolis-bold text-3xl mb-4">How to Contact Us</h2>
+      <p className="font-metropolis text-base">
+        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
+        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
+        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
+        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
+        venenatis. Donec fringilla augue non neque consectetur finibus sed et
+        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
+        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      </p>
+    </div>
+  );
+};
+
+export default Page;
