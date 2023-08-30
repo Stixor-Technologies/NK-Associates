650fd49519f31e8748d2239aa7a2f9572934f9d4
OmerKhanStix <omer.khan@stixor.com>
2023-08-30 16:12:01 +0500

    added skelton color, changed skeleton

--- apps/nk-associates-frontend/components/skeletons/projects/project-card-skeleton.tsx
+++ apps/nk-associates-frontend/components/skeletons/projects/project-card-skeleton.tsx
@@ -5,33 +5,15 @@ interface CardProps {
   actHome?: boolean;
 }
 
-const ProjectCardSkeleton: React.FC<CardProps> = ({
-  actHome,
-  primaryColor = true,
-}) => {
+const ProjectCardSkeleton: React.FC<CardProps> = ({ actHome }) => {
   const cardHeight = actHome
     ? "h-[35rem] sm:h-[16.875rem] md:h-[20.625rem] lg:h-[25rem]"
     : "h-[35rem] sm:h-[21rem] md:h-[26rem] lg:h-[31.25rem]";
 
-  const flexDirection = primaryColor ? "sm:flex-row-reverse" : "sm:flex-row";
-
   return (
     <div
-      className={`project-card mb-[2.3rem] flex w-full flex-col overflow-hidden rounded-2xl shadow-md last-of-type:mb-4  md:mb-[4.5rem] ${flexDirection} md:rounded-3xl ${cardHeight} animate-pulse`}
-    >
-      <div className="h-full min-h-[21rem] w-full sm:h-auto sm:w-[65%] bg-nk-light-gray"></div>
-      <div className="flex flex-col px-4 py-3.5 sm:w-[35%] md:p-6 bg-nk-gray">
-        <div className="h-6 bg-nk-light-gray rounded-full w-1/2 mb-4"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-full mb-4"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-full mb-4"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-full mb-4"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-full mb-4"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-11/12 mb-4"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-1/2 mt-8"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-1/3 mt-4"></div>
-        <div className="h-4 bg-nk-light-gray rounded-full w-1/2 mt-4"></div>
-      </div>
-    </div>
+      className={`project-card mb-[2.3rem] flex w-full flex-col overflow-hidden rounded-2xl shadow-md last-of-type:mb-4  md:mb-[4.5rem] md:rounded-3xl ${cardHeight} animate-pulse bg-nk-skeleton-grey`}
+    ></div>
   );
 };
 
--- apps/nk-associates-frontend/components/skeletons/property/property-card-skeleton.tsx
+++ apps/nk-associates-frontend/components/skeletons/property/property-card-skeleton.tsx
@@ -7,12 +7,12 @@ const PropertySkeleton = () => {
         role="status"
         className="space-y-8 animate-pulse md:space-y-0 flex-col md:items-center"
       >
-        <div className="flex items-center justify-center w-full h-[17.5rem] bg-nk-light-gray rounded-xl"></div>
+        <div className="flex items-center justify-center w-full h-[17.5rem] bg-nk-skeleton-grey rounded-xl"></div>
         <div>
-          <div className="h-6 bg-nk-light-gray rounded-full w-full my-3"></div>
-          <div className="h-4 bg-nk-light-gray rounded-full w-1/3 my-3.5"></div>
-          <div className="h-4 bg-nk-light-gray rounded-full w-1/3 mb-3.5"></div>
-          <div className="h-4 bg-nk-light-gray rounded-full w-1/3 mb-3.5"></div>
+          <div className="h-6  bg-nk-skeleton-grey rounded-full w-full my-3"></div>
+          <div className="h-4  bg-nk-skeleton-grey rounded-full w-1/3 my-3.5"></div>
+          <div className="h-4  bg-nk-skeleton-grey rounded-full w-1/3 mb-3.5"></div>
+          <div className="h-4  bg-nk-skeleton-grey rounded-full w-1/3 mb-3.5"></div>
         </div>
         <span className="sr-only">Loading...</span>
       </div>
