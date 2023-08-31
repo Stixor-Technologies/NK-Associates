5684b887efd67179bd84627a9e0105dc52743b5b
OmerKhanStix <omer.khan@stixor.com>
2023-08-31 13:03:41 +0500

    added generic content

--- apps/nk-associates-frontend/app/privacy-policy/page.tsx
+++ apps/nk-associates-frontend/app/privacy-policy/page.tsx
@@ -3,102 +3,144 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="block p-8 text-nk-dark-gray title-trigger">
+    <div className="block p-8 text-nk-dark-gray container">
       <h1 className="font-metropolis-bold text-center text-5xl md:p-[2rem] md:pb-[1rem]">
         Privacy Policy
       </h1>
       <hr className="h-px mb-5 bg-nk-gray border"></hr>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
-        This policy describes the sources of your personal information that we
-        may collect from you or that you may provide to us, our purposes for
-        collecting and processing your information, and our practices for
-        collecting, using, maintaining, retaining, protecting, and disclosing
-        that information.
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        At NK Developers, accessible from nkassociates.co, one of our main
+        priorities is the privacy of our visitors. This Privacy Policy document
+        contains types of information that is collected and recorded by NK
+        Developers and how we use it
       </p>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
         If you have any questions or concerns about this privacy policy or your
         personal information, please contact us using the details provided in
         the Section – ‘How do you contact us?’.
       </p>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        This Privacy Policy applies only to our online activities and is valid
+        for visitors to our website with regards to the information that they
+        shared and/or collect in NK Developers. This policy is not applicable to
+        any information collected offline or via channels other than this
+        website.’.
+      </p>
 
-      <h2 className="font-metropolis-bold text-3xl mb-4">
-        Sources of Personal Data
-      </h2>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
-        This section outlines the sources from which we collect your personal
-        data.
+      <h2 className="font-metropolis-semibold text-3xl mb-4">Consent</h2>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        By using our website, you hereby consent to our Privacy Policy and agree
+        to its terms
       </p>
 
       <h2 className="font-metropolis-semibold text-3xl mb-4">
-        Information We Collect and How We Use It
+        Updates on Policy
       </h2>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
-        This section explains the types of information we collect about you and
-        how we use it for various purposes.
-      </p>
-
-      {/* wating on other sections */}
-
-      <h2 className="font-metropolis-bold text-3xl mb-4">Updates on Policy</h2>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
         This policy may change from time to time. Your continued use of our
         platforms after we make changes is deemed to be acceptance of those
         changes, so please check the policy periodically for updates.
       </p>
 
-      <h2 className="font-metropolis-bold text-3xl mb-4">How to Contact Us</h2>
-      <p className="font-metropolis text-base">
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
+        How to Contact Us
+      </h2>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
         In case of any queries related to this policy, you can contact our Data
-        Privacy Office in one of the following ways.
+        Privacy Office.
       </p>
-      <h2 className="font-metropolis-bold text-3xl mb-4">
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
         Sources of Personal Data
       </h2>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
-        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
-        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
-        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
-        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
-        venenatis. Donec fringilla augue non neque consectetur finibus sed et
-        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
-        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        We collect personal data from various sources in order to provide our
+        services. For instance, when you create an account on our platform, we
+        gather information such as your name, email address, and other contact
+        details. Additionally, we may collect data from your interactions with
+        our website, such as the pages you visit and the actions you take. Your
+        personal data may also be acquired from third-party partners and service
+        providers that help us offer our services to you effectively. This data
+        may include demographic information and preferences that enable us to
+        tailor our offerings to your needs. Rest assured, we handle all
+        collected data with care, in line with our Privacy Policy.
       </p>
 
       <h2 className="font-metropolis-semibold text-3xl mb-4">
         Information We Collect and How We Use It
       </h2>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
-        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
-        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
-        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
-        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
-        venenatis. Donec fringilla augue non neque consectetur finibus sed et
-        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
-        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        At our company, we take the privacy of your data seriously. We gather
+        information to offer you a seamless experience while using our services.
+        This includes data like your name, email address, and contact details,
+        which you provide when signing up for an account or interacting with our
+        platform. We also collect usage data, such as the features you engage
+        with and the pages you visit, to better understand user preferences and
+        enhance our offerings. It's important to note that we adhere to strict
+        security measures to protect your data from unauthorized access or
+        misuse. We do not share your personal information with third parties
+        without your consent, except when required by law. By utilizing your
+        data responsibly, we aim to continuously improve our services and
+        provide you with a tailored experience that meets your needs and
+        expectations.
+      </p>
+      <h2 className="font-metropolis-semibold text-3xl mb-4">Data Usage</h2>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        We use the data collected to provide and improve our services. This may
+        include personalization of content, analyzing user trends, and enhancing
+        our platform's functionality. The insights gained from data analysis
+        help us optimize user experience and develop new features that align
+        with your preferences. We never use your data for purposes other than
+        improving our services, and your personal information remains protected
+        and secure.
       </p>
 
-      {/* wating on other sections */}
-
-      <h2 className="font-metropolis-bold text-3xl mb-4">Updates on Policy</h2>
-      <p className="font-metropolis text-base mb-4 text-[0.875rem] md:text-[1.25rem]">
-        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
-        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
-        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
-        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
-        venenatis. Donec fringilla augue non neque consectetur finibus sed et
-        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
-        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
+        Cookies and Tracking Technologies
+      </h2>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        Our website may utilize cookies and tracking technologies to enhance
+        your browsing experience. These tools allow us to remember your
+        preferences, offer relevant content, and analyze user behavior. Cookies
+        do not compromise your privacy, and you can manage their usage through
+        your browser settings. By accepting cookies, you help us customize your
+        experience and deliver content that aligns with your interests.
+      </p>
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
+        Information Sharing and Disclosure
+      </h2>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        We understand the importance of your data's privacy. We do not sell,
+        trade, or rent your personal information to third parties for marketing
+        purposes. However, in certain cases, we may share your information with
+        trusted partners who assist us in delivering our services. This includes
+        providing customer support, analyzing user data, and improving our
+        offerings. Any sharing of data is conducted in compliance with legal
+        requirements and our Privacy Policy.
       </p>
 
-      <h2 className="font-metropolis-bold text-3xl mb-4">How to Contact Us</h2>
-      <p className="font-metropolis text-base">
-        Nullam eget diam et massa dignissim bibendum nec nec ipsum. Quisque vel
-        condimentum metus. Nunc pharetra luctus tortor in aliquam. Nulla
-        ultricies eleifend elementum. Pellentesque at pellentesque libero. In
-        viverra magna sed nibh pharetra mollis. Nam condimentum luctus
-        venenatis. Donec fringilla augue non neque consectetur finibus sed et
-        purus. Maecenas id nisl et magna condimentum rutrum nec eu erat.
-        Vestibulum et orci non mauris laoreet pretium sit amet vel risus
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
+        Data Security and Retention
+      </h2>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        Safeguarding your data is our top priority. We implement
+        industry-standard security measures to protect your personal information
+        from unauthorized access, alteration, or disclosure. Our data retention
+        practices align with the purpose for which data was collected. We retain
+        your information only as long as necessary to fulfill these purposes. In
+        cases where data is no longer needed, it will be securely deleted or
+        anonymized to ensure your privacy.
+      </p>
+
+      <h2 className="font-metropolis-semibold text-3xl mb-4">
+        Your Privacy Rights
+      </h2>
+      <p className="font-metropolis text-base mb-6 text-[0.875rem] md:text-[1.25rem]">
+        We respect your privacy rights and provide options for managing your
+        data. You have the right to access, correct, or delete your personal
+        information. If you wish to exercise these rights or have questions
+        about your data's handling, please contact our Data Privacy Office using
+        the provided contact details. We are committed to assisting you and
+        addressing your concerns.
       </p>
     </div>
   );
