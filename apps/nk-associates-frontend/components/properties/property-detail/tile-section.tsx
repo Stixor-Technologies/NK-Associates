4bd5ffcec531ac381b23856b90ac999ae0c4a378
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-09-06 12:48:22 +0500

    fixed the trigger issue on detail page

--- apps/nk-associates-frontend/components/properties/property-detail/tile-section.tsx
+++ apps/nk-associates-frontend/components/properties/property-detail/tile-section.tsx
@@ -34,7 +34,8 @@ const TileSection: FC<TileSectionProps> = ({
         scrollTrigger: {
           id: "property-tile-trigger",
           trigger: divElement,
-          start: "top 75%",
+          start: "top 88%",
+          markers: true,
         },
       });
     }
