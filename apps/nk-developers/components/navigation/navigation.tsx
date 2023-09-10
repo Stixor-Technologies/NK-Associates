6eb7b8de2264d3a7bb5f67a51b71e9ec0555f248
HashaamBinEjaz <139843321+HashaamBinEjaz@users.noreply.github.com>
2023-09-10 18:01:17 +0500

    Added priority tag to first image and removed search icon from navbar

--- apps/nk-developers/components/navigation/navigation.tsx
+++ apps/nk-developers/components/navigation/navigation.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "../../public/assets/icons/nk-logo.svg";
-import SearchIcon from "../../public/assets/icons/search-icon.svg";
 
 const Navigation = () => {
   return (
@@ -16,9 +15,6 @@ const Navigation = () => {
             </Link>
           </div>
           <div className="flex justify-between">
-            <button className="hidden pr-8 md:flex md:items-center mt-4">
-              <Image src={SearchIcon} alt="Search" width={39} height={39} />
-            </button>
             <Link
               href="https://develop.d2vr74cwsk1kb.amplifyapp.com/contact"
               className=" mr-2 mt-4 h-10 cursor-pointer flex flex-row items-center rounded-full border border-nk-red bg-transparent px-6 text-center font-metropolis capitalize text-nk-red md:z-50"
