14f23272f778f439a74d99c27ba31400c4037cea
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-09-25 13:39:26 +0500

    updated the name of collection

--- apps/nk-associates-cms/src/api/mobile-map/routes/mobile-map.ts
+++
@@ -1,7 +0,0 @@
-/**
- * mobile-map router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::mobile-map.mobile-map');
--- apps/nk-associates-cms/src/api/mobile-map/services/mobile-map.ts
+++
@@ -1,7 +0,0 @@
-/**
- * mobile-map service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::mobile-map.mobile-map');
