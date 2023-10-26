a8e2a3817be84700d2a439ee05edff237fadd47f
fawadmehmood1 <fawad.mehmood@stixor.com>
2023-10-26 12:32:24 +0500

    recieved code from url

--- apps/nk-associates-frontend/app/NewPassword/page.tsx
+++ apps/nk-associates-frontend/app/NewPassword/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import LinkButton from "../../components/button/link-button";
 
 const PasswordRedirect = () => {
   const searchParams = useSearchParams();
@@ -9,8 +10,19 @@ const PasswordRedirect = () => {
 
   console.log(code);
 
+  useEffect(() => {
+    console.log("useEffect called");
+    window.location.href = "nkapp://app/NewPassword?code=1234";
+  }, []);
+
   return (
     <div className="min-h-[50vh] flex items-center justify-center">
+      <LinkButton
+        text="open app"
+        clickEvent={() => {
+          window.location.href = "nkapp://app/NewPassword?code=1234";
+        }}
+      />
       {/* {isMobile && (
         <div className="flex flex-col justify-center">
           <h3 className="font-metropolis-semibold text-2xl max-w-md text-nk-red mb-4">
